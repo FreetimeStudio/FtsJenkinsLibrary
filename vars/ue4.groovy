4511e746d874962fd9ffe34147a42a3c4784b4c9
Pascal Krabbe <pkrabbe@freetimestudio.net>
2023-08-05 19:21:34 +0200

    * Trying to fix test running

--- vars/ue4.groovy
+++ vars/ue4.groovy
@@ -250,7 +250,7 @@ def validateAssets(Map config = [:]) {
 def runProjectTests(Map config = [:]) {
     lock(resource: "UnrealBuildTool-${NODE_NAME}") {
         def editorCMD = getEditorCMDPath(config)
-        platform.executeScript("\"${editorCMD}\" \"${config.projectPath}\" -game -ExecCmds=\"Automation RunTests Project\" -unattended -nopause -NullRHI -testexit=\"Automation Test Queue Empty\" -log -ReportOutputPath=\"${config.reportPath}\"", 'Run Project Automation Tests')
+        platform.executeScript("\"${editorCMD}\" \"${config.projectPath}\" -game -ExecCmds=\"Automation RunTests Project;Quit\" -unattended -NoSound -nopause -nosplash -NullRHI  -testexit=\"Automation Test Queue Empty\" -log -ReportOutputPath=\"${config.reportPath}\"", 'Run Project Automation Tests')
     }
 }
 
